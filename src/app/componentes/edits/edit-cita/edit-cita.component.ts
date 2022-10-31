ef41edcce1ff453d8cc57ae22fa7635bce8ce1a7
arodoo <haro.em@hotmail.com>
2022-10-30 21:40:00 -0600

    Visual 80% y formulario funcional 50%

--- src/app/componentes/edits/edit-cita/edit-cita.component.ts
+++ src/app/componentes/edits/edit-cita/edit-cita.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-cita',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-cita.component.css']
 })
 export class EditCitaComponent implements OnInit {
-
-  constructor() { }
+  editCitaForm!: FormGroup;
+  constructor(private readonly fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.editCitaForm = this.initForm();
   }
 
+  onSubmit(): void {
+    console.log('Form ->');
+  }
+
+  initForm(): FormGroup {
+    //Se declaran las propiedades del formulario
+    //primer argumento el valor por defecto, el segundo validaciones con validators
+    return this.fb.group({
+      fecha: ['', Validators.required],
+      lugar: ['', Validators.required],
+      consulta: ['', Validators.required],
+      medico: ['', Validators.required],
+      motivo: ['', Validators.required]
+    })
+  }
 }
